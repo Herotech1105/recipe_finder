86b0a8a9340afe6feb9b85363423a546ac041760
Lennart Esch <lennart.esch@innosystec.de>
2026-04-29 10:18:32 +0200

    add meal_type to recipe in database setup

--- backend_api/src/main/resources/db-migration/V1.0.0__initial_setup.sql
+++ backend_api/src/main/resources/db-migration/V1.0.0__initial_setup.sql
@@ -1,19 +1,27 @@
-CREATE TYPE unit AS ENUM
-    (
-        'ml',
-        'g',
-        'Stk.',
-        'TL',
-        'EL',
-        ''
-        );
+CREATE TYPE unit AS ENUM (
+    'ML',
+    'G',
+    'STK.',
+    'TL',
+    'EL',
+    ''
+    );
+
+CREATE TYPE meal_type AS ENUM (
+    'MAIN',
+    'STARTER',
+    'SNACK',
+    'DESSERT',
+    'NONE'
+    );
 
 CREATE TABLE recipes
 (
     id          serial8 primary key,
-    name        text    NOT NULL,
-    preparation text    NOT NULL,
-    author_id   serial8 NOT NULL
+    name        text      NOT NULL,
+    preparation text      NOT NULL,
+    author_id   serial8   NOT NULL,
+    category    meal_type NOT NULL default 'NONE'
 );
 
 CREATE TABLE ingredients
@@ -32,10 +40,10 @@ CREATE TABLE users
 CREATE TABLE storage
 (
     id            serial8 primary key,
-    user_id       serial8,
-    ingredient_id serial8,
-    amount        float,
-    unit          unit
+    user_id       serial8 NOT NULL,
+    ingredient_id serial8 NOT NULL,
+    amount        float   NOT NULL,
+    unit          unit    NOT NULL
 );
 
 CREATE TABLE recipe_ingredients

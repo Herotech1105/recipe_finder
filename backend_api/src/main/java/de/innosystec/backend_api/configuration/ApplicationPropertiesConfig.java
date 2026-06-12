96373d07ea85808aa2e17b2066819644cdfe63fb
Lennart Esch <lennart.esch@innosystec.de>
2026-06-12 09:49:07 +0200

    add Config Class for general application properties

---
+++ backend_api/src/main/java/de/innosystec/backend_api/configuration/ApplicationPropertiesConfig.java
@@ -0,0 +1,36 @@
+package de.innosystec.backend_api.configuration;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+
+@Configuration
+@PropertySource("classpath:application.properties")
+@ConfigurationProperties(prefix = "spring.application")
+public class ApplicationPropertiesConfig {
+
+    @Value("name")
+    private String name;
+
+    @Value("version")
+    private String version;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getVersion() {
+        return version;
+    }
+
+    public void setVersion(String version) {
+        this.version = version;
+    }
+}
+
+

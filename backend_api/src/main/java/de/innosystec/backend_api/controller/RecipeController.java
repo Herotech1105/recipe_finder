6dcce70e208608bc7c9fe00574454fa8c7377f4d
Lennart Esch <lennart.esch@innosystec.de>
2026-06-25 13:45:17 +0200

    add http status codes to recipe controller

--- backend_api/src/main/java/de/innosystec/backend_api/controller/RecipeController.java
+++ backend_api/src/main/java/de/innosystec/backend_api/controller/RecipeController.java
@@ -5,6 +5,7 @@ import de.innosystec.backend_api.model.recipe.RecipeListItemDTO;
 import de.innosystec.backend_api.model.recipe.RecipeRequestDTO;
 import de.innosystec.backend_api.service.RecipeService;
 import jakarta.validation.Valid;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -28,18 +29,21 @@ public class RecipeController {
         return service.getALlRecipes();
     }
 
+    @ResponseStatus(HttpStatus.CREATED)
     @PostMapping
     public void createRecipe(@RequestBody @Valid RecipeRequestDTO recipeRequest,
                              @RequestHeader("Authorization") String jwtToken) {
         service.createRecipe(recipeRequest, jwtToken.substring(7));
     }
 
+    @ResponseStatus(HttpStatus.NO_CONTENT)
     @DeleteMapping("/{id}")
     public void deleteRecipe(@PathVariable Long id,
                              @RequestHeader("Authorization") String jwtToken) {
         service.deleteRecipe(id, jwtToken.substring(7));
     }
 
+    @ResponseStatus(HttpStatus.NO_CONTENT)
     @PutMapping("/{id}")
     public void updateRecipe(@PathVariable Long id,
                              @RequestBody @Valid RecipeRequestDTO recipeRequest,

3444aefe8c2d0f9e354362da02504cafe4e7f71c
Lennart Esch <lennart.esch@innosystec.de>
2026-06-27 16:17:47 +0200

    bugfix: set Ingredients path from /api/ingredients to /ingredients to prevent error in vite reverse proxy

--- backend_api/src/main/java/de/innosystec/backend_api/controller/StorageController.java
+++ backend_api/src/main/java/de/innosystec/backend_api/controller/StorageController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/api/storage")
+@RequestMapping("/storage")
 public class StorageController {
 
     private final StorageService storageService;
